d9e73f3b765a061fea0d23da9b6cc4f4b72e0080
jdddog <jamie.diprose@gmail.com>
2014-08-18 18:27:53 +0800

    Made message names similar to dynamixel_msgs stack Specify arguments with parameters Multiple pololu boards can be controlled Added example launch files Can set motor position using joint name as argument (rather tha channel name)
    Needs to be tested on Indigo

--- src/ros_pololu_servo_node.cpp
+++ src/ros_pololu_servo_node.cpp
@@ -1,112 +1,125 @@
 #include <ros/ros.h>
 #include <vector>
-#include <ros_pololu_servo/servo_pololu.h>
-//#include <ros_pololu_servo/pololu_state.h>
-#include <ros_pololu_servo/motor.h>
-#include <ros_pololu_servo/pololu_info.h>
+#include <ros_pololu_servo/MotorCommand.h>
+#include <ros_pololu_servo/MotorState.h>
+#include <ros_pololu_servo/MotorStateList.h>
+#include <math.h>
+#include <map>
 #include "PolstroSerialInterface.h"
-
 #include "yaml-cpp/yaml.h"
 
-const unsigned int baudRate = 115200;//38400
-const float pi = 3.141592653589793f;
-const unsigned int channelMinValue = 4000;
-const unsigned int channelMaxValue = 8000;
-const unsigned int channelValueRange = channelMaxValue - channelMinValue;
-const unsigned int signalPeriodInMs = 2000;
-Polstro::SerialInterface* serialInterface;
-std::string portName = "/dev/ttyACM0";
-ros_pololu_servo::servo_pololu msgTemp,msgs;
-ros_pololu_servo::motor motor;
-ros_pololu_servo::pololu_info inf;
-YAML::Node config = YAML::LoadFile("/rsm/pololu.yaml");
+using namespace ros_pololu_servo;
+using namespace std;
+
 ros::Publisher pub;
-/*
-bool status(ros_pololu_servo::pololu_state::Request  &req, 
-ros_pololu_servo::pololu_state::Response &res)
+YAML::Node config;
+Polstro::SerialInterface* serial_interface;
+string pololu_config_dir, port_name, pololu_namespace;
+int baud_rate, channel_min_value, channel_max_value, channel_value_range, rate_hz;
+MotorStateList motor_state_list;
+map<string, int> joints;
+
+void publish_motor_state()
 {
-	//
-	unsigned char channelNumber=req.qid;
-	bool ret;
-	unsigned short position;
-	res.id;
-	serialInterface->getPositionCP( channelNumber, position );
-	res.angle=(((float)(position-channelMinValue)/(float)channelValueRange)-0.5)*pi;
-	ROS_INFO("getPositionCP(%d) (ret=%d position=%d)\n", channelNumber, ret, position );
-	return true;
-}
-*/
-void pub_motors()
-{
-	inf.info.clear();
-	for (std::size_t i=0; i<config.size(); i++) 
+	motor_state_list.motor_states.clear();
+
+	for (std::size_t i = 0; i < config.size(); i++)
 	{
-		//std::cout << config[i]["id"].as<std::string>() << "\n";
-	unsigned char channelNumber=atoi(config[i]["id"].as<std::string>().c_str());
-	unsigned short position;
-	serialInterface->getPositionCP( channelNumber, position );
-	float angle=(((float)(position-channelMinValue)/(float)channelValueRange)-0.5)*pi;
-	ROS_INFO("getPositionCP(%d) (position=%d)\n", channelNumber, position );
-	motor.id=channelNumber;
-	motor.name=config[i]["name"].as<std::string>();
-	motor.position=angle;
-	inf.info.push_back(motor);
+        unsigned char channel_number = atoi(config[i]["id"].as<string>().c_str());
+        unsigned short position;
+
+        serial_interface->getPositionCP(channel_number, position);
+        float angle = (((float)(position - channel_min_value) / (float)channel_max_value) - 0.5) * M_PI;
+
+        ROS_INFO("channel_number: %d, position: %d", channel_number, position);
+        MotorState motor;
+        motor.id = channel_number;
+        motor.name = config[i]["name"].as<string>();
+        motor.position = angle;
+        motor_state_list.motor_states.push_back(motor);
 	}
-	pub.publish(inf);
-}
-/*
-void timerCallback(const ros::TimerEvent& e)
-{
-	pub_motors();
-}
-*/
-void CommandCallback(const ros_pololu_servo::servo_pololu::ConstPtr& msg)
-{
-	//ROS_INFO("I heard: [%s]", msg->data.c_str());
-	//msgTemp=(*msg);
-	unsigned char channelNumber=msg->id;
-	if (msg->speed>0)serialInterface->setSpeedCP( channelNumber, msg->speed );
-	if (msg->acceleration>0)serialInterface->setAccelerationCP( channelNumber, msg->acceleration );
-	if ((msg->angle>=-pi/2)&&(msg->angle<=pi/2)){
-		//
-		int i=((msg->angle+pi/2.0)/pi)*(float)channelValueRange+(float)channelMinValue;
-		ROS_INFO("motor %d : position=%d",channelNumber,i);
-		if (i>=channelMinValue && i<=channelMaxValue)
-		serialInterface->setTargetCP( channelNumber, i );
-	}
+
+	pub.publish(motor_state_list);
 }
 
+void motor_command_callback(const MotorCommand::ConstPtr& msg)
+{
+    int channel_number = joints[msg->joint_name];
+
+	if ((msg->position >= -M_PI/2) && (msg->position <= M_PI/2))
+	{
+		int i = ((msg->position + M_PI/2.0) / M_PI) * (float)channel_value_range + (float)channel_min_value;
+		ROS_INFO("joint_name: %s, id: %d, position: %d", msg->joint_name.c_str(), channel_number, i);
+
+		if (i >= channel_min_value && i<= channel_max_value)
+		{
+		    serial_interface->setSpeedCP(channel_number, msg->speed);
+            serial_interface->setAccelerationCP(channel_number, msg->acceleration);
+		    serial_interface->setTargetCP(channel_number, i);
+        }
+	}
+}
 
 int main(int argc,char**argv)
 {
-	ros::init(argc, argv, "pololu_servo");
+	ros::init(argc, argv, "pololu_servo", ros::init_options::AnonymousName);
 	ros::NodeHandle n;
-	ROS_INFO("Creating serial interface '%s' at %d bauds\n", portName.c_str(), baudRate);
-	serialInterface = Polstro::SerialInterface::createSerialInterface( portName, baudRate );
-	if ( !serialInterface->isOpen() )
+	ros::NodeHandle nh("~");
+
+    // Load parameters
+	ROS_INFO("Loading parameters");
+
+	if (nh.hasParam("pololu_config"))
+    {
+        nh.getParam("pololu_config", pololu_config_dir);
+        config = YAML::LoadFile(pololu_config_dir);
+
+        for (std::size_t i = 0; i < config.size(); i++)
+	    {
+	        string joint_name = config[i]["name"].as<string>();
+	        int channel_number = i;
+	        joints[joint_name] = channel_number;
+        }
+    }
+    else
+    {
+        ROS_ERROR("pololu_config file not specified, exiting");
+        return EXIT_FAILURE;
+    }
+
+    nh.param<string>("pololu_namespace", pololu_namespace, "pololu");
+    nh.param<string>("port_name", port_name, "/dev/ttyACM0");
+    nh.param<int>("baud_rate", baud_rate, 115200);
+    nh.param<int>("channel_min_value", channel_min_value, 4000);
+    nh.param<int>("channel_max_value", channel_max_value, 8000);
+    nh.param<int>("rate_hz", rate_hz, 10);
+    channel_value_range = channel_max_value - channel_min_value;
+
+    // Create serial interface
+	ROS_INFO("Creating serial interface '%s' at %d bauds\n", port_name.c_str(), baud_rate);
+	serial_interface = Polstro::SerialInterface::createSerialInterface(port_name, baud_rate);
+	if (!serial_interface->isOpen())
 	{
-		ROS_ERROR("Failed to open interface\n");
-		return -1;
+		ROS_ERROR("Failed to open interface, exiting");
+		return EXIT_FAILURE;
 	}
-	
-	pub = n.advertise<ros_pololu_servo::pololu_info>("pololu_info", 100);
-	////ros::Timer timer = n.createTimer(ros::Duration(0.5), timerCallback);
-	ros::Subscriber sub = n.subscribe("/cmd_pololu", 20, CommandCallback);
-	//ros::ServiceServer service = n.advertiseService("pololu_status", status);
+
+    // Setup publisher and subscriber
+	pub = n.advertise<MotorStateList>(pololu_namespace + "/motor_states", 100);
+	n.subscribe(pololu_namespace + "/motor_command", 20, motor_command_callback);
+
+    // Publish motor state
     ROS_INFO("Ready...");
-	//
-	////ros::spin();
-	
-	ros::Rate loop_rate(10);
+    ros::Rate rate(rate_hz);
 	while(ros::ok())
 	{
+	    publish_motor_state();
 		ros::spinOnce();
-        loop_rate.sleep();
-        pub_motors();
+        rate.sleep();
 	}
-	
+
 	ROS_INFO("Deleting serial interface...");
-	delete serialInterface;
-	serialInterface = NULL;
-	return 0;
+	delete serial_interface;
+	serial_interface = NULL;
+	return EXIT_SUCCESS;
 }
